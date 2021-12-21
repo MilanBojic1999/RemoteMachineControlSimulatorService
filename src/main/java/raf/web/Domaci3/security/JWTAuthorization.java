fa1057aa3c29640cd912eddbf5f0c8f2233b1ba2
milan boi <mbojic12@gmail.com>
2021-12-21 21:46:13 +0100

    Configure security

--- src/main/java/raf/web/Domaci3/security/JWTAuthorization.java
+++ src/main/java/raf/web/Domaci3/security/JWTAuthorization.java
@@ -1,9 +1,13 @@
 package raf.web.Domaci3.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import raf.web.Domaci3.repositories.IUserRepository;
+import raf.web.Domaci3.services.UserService;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -13,16 +17,31 @@ import java.io.IOException;
 
 public class JWTAuthorization extends BasicAuthenticationFilter {
 
-    private IUserRepository repository;
+    private UserService service;
+    private JwtUtil jwtUtil;
 
     @Autowired
-    public JWTAuthorization(AuthenticationManager authenticationManager, IUserRepository repository) {
+    public JWTAuthorization(AuthenticationManager authenticationManager, UserService service) {
         super(authenticationManager);
-        this.repository = repository;
+        this.service = service;
+        this.jwtUtil = new JwtUtil();
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        super.doFilterInternal(request, response, chain);
+        String authHeader = request.getHeader(Tokens.HEADER);
+        System.out.println("+++"+request.getRequestURI()+"-----"+request.getRemoteUser()); // iz getRequestURI izvalcimo tačan zahtev
+
+        String jwt = null;
+        String email = null;
+
+        if(authHeader != null && authHeader.startsWith(Tokens.PREFIX)) {
+            jwt = authHeader.replace(Tokens.PREFIX,"");
+            email = jwtUtil.extractEmail(jwt);
+        }
+
+        UserDetails userDetails = service.loadUserByUsername(email);
+
+        chain.doFilter(request, response);
     }
 }
